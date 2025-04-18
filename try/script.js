1861196f548f857421c893df02a6041a49a2dfb8
prateekrajgautam <prateekrajgautam@gmail.com>
2025-04-18 18:27:19 +0530

    try working, examples Dict needs to be updated

--- try/script.js
+++ try/script.js
@@ -34,12 +34,18 @@ const loadSkelton =()=> {
     // )
 // //optionbar
   append(instructions,gen(div,"optionbar","","optionbar"))
-  append(optionbar,gen(span,'save',"Save","button",{onclick:"saveImage()"}))
-  append(optionbar,gen(span,'try',"Try","button",{onclick:"updateOutput()"}))
-  append(optionbar,gen(span,'init',"Init","button",{onclick:"initsetup()"}))
+  append(optionbar,gen(div,"optioncontainer","","optioncontainer"))
+  
+  append(optioncontainer,gen(span,'save',"Save as ","button",{onclick:"saveImage()"}))
+  append(save,gen(span,'fileName',"Demo","",{contenteditable:true,onclick:"event.stopPropagation()"},'fileName'))
+  
+  append(optioncontainer,gen(span,'try',"Try","button",{onclick:"updateOutput()"}))
+  append(optioncontainer,gen(span,'Example',"Example","button",{onclick:"example()"}))
+  append(optioncontainer,gen(span,'Reset',"Reset","button",{onclick:"resetSetup()"}))
 
 
   append(main, gen(div, "saveCanvas", gen(h1,"","Learn GeneratorJS",'canvasHeading'), "save canvas"));
+  append("#saveCanvas",gen(p,"comment","",'comment'))
   append(saveCanvas, gen(section, "gridRoot", "", "gridRoot"));
 
   var blocks = "beforehtml".split(",");
@@ -47,7 +53,7 @@ const loadSkelton =()=> {
     var id = block + "-block";
     var boxid = block + "-box";
     append(gridRoot, gen(div, id, "", block + ",column"));
-    append(`#${id}`, gen(h3, "", "HTML Before","block-heading"));
+    append(`#${id}`, gen(h3, "", "HTML Before (DOM)","block-heading"));
     append(`#${id}`, gen(div, `${boxid}`, "", "box"));
     append(
       `#${boxid}`,
@@ -62,7 +68,7 @@ const loadSkelton =()=> {
   blocks.forEach((block) => {
     var id = block + "-block";
     append(gridRoot, gen(div, id, "", block + ",column"));
-    append(`#${id}`, gen(h3, "", "JS Code","block-heading"));
+    append(`#${id}`, gen(h3, "", "JS Code (javascript)","block-heading"));
     append(
       `#${id}`,
       gen(code, `${block}-code`, block, "code,scrolly", {
@@ -76,7 +82,7 @@ const loadSkelton =()=> {
   blocks.forEach((block) => {
     var id = block + "-block";
       append(gridRoot, gen(div, id, "", block + ",column"));
-    append(`#${id}`, gen(h3, "", "HTML After","block-heading"));
+    append(`#${id}`, gen(h3, "", "HTML After (DOM)","block-heading"));
     append(
       `#${id}`,
       gen(pre,"",gen(code, `${block}-code`, block, "code,scrolly", {
@@ -91,7 +97,7 @@ const loadSkelton =()=> {
   blocks.forEach((block) => {
     var id = block + "-block";
     append(gridRoot, gen(div, id, "", block + ",column"));
-    append(`#${id}`, gen(h3, "", "Render","block-heading"));
+    append(`#${id}`, gen(h3, "", "HTML Preview","block-heading"));
     append(
       `#${id}`,
       gen(div, `${block}-code`, block, "code,scrolly", {
@@ -112,7 +118,7 @@ loadSkelton()
 
 
 
-function initsetup(beforestring= `<div id="testmain">Test Main Text</div>`,  codeString= `append("#testmain",gen("p","","hi"))`){
+function resetSetup(beforestring= `<div id="testmain">Test Main Text</div>`,  codeString= `append("#testmain",gen("p","","hi"))`,comment=""){
   // log(beforestring)
   
   append(`#beforehtml-code`,beforestring.replace(/</g, "\n&lt;").replace(/>/g, "&gt;\n").replace(/\n+/g, "\n").replace(/^\n/g, "\n"),'o')
@@ -125,6 +131,9 @@ function initsetup(beforestring= `<div id="testmain">Test Main Text</div>`,  cod
   append(`#output-code`,readbefore,"o")
 
   // append("#testmain","Test Main Text","o"); 
+  if (comment.length > 0) {
+    append(`#comment`,comment,"o")
+  }
 
 }
 
@@ -132,7 +141,7 @@ var beforestring=`<div id="testmain">Test Main Text</div>`
 var codeString = `append("#testmain",gen("p","","hi"))`
 
 
-initsetup(beforestring,codeString)
+resetSetup(beforestring,codeString)
 
 
 
@@ -147,10 +156,10 @@ function updateOutput() {
 
     var readbefore = grab(`#beforehtml-code`)[0].innerText
     var jscode = grab(`#jscode-code`)[0].innerText
-    initsetup(readbefore,jscode)
+    resetSetup(readbefore,jscode)
     append(`#output-code`,readbefore,"o")
     eval(jscode)
-    // initsetup()
+    // resetSetup()
 
     var readafter = grab(`#output-code`)[0].innerHTML
     append(`#afterhtml-code`,readafter.replace(/</g, "\n&lt;").replace(/>/g, "&gt;\n").replace(/\n+/g, "\n").replace(/^\n/g, "\n"),"o")
@@ -159,22 +168,87 @@ function updateOutput() {
 
 
 
+
+var exampleNo = 0
+function example() {
+
+  var examples = [
+    {
+      beforestring: `<div id="testmain">Test Main Text</div>`,
+      codeString: `append("#testmain",gen("p","","hi"))`,
+      comment: `// example showing append`,
+      // afterstring: `<div id="testmain">Test Main Text</div><p>hi</p>`,
+  
+    },
+    {
+      beforestring: `<div id="testmain">Test Main Text</div>`,
+      codeString: `append("#testmain",gen("p","","hi")) <br>\/\/ example showing append after`,
+      // afterstring: `<div id="testmain">Test Main Text</div><p>hi</p>`,  
+      comment: `// example showing append after`,
+    },
+    {
+      beforestring: `<div id="testmain">Test Main Text</div>`,
+      codeString: `append("#testmain",gen("p","","hi"),"b") <br>\/\/append("#testmain",gen("p","","hi"),"before")  <br>\/\/ example showing append before`,
+      comment: `// example showing append before`,
+      // afterstring: `<div id="testmain">Test Main Text</div><p>hi</p>`,  
+    }
+  ]
+  
+  if (exampleNo > examples.length - 1) {
+    exampleNo = 0
+  }
+  var beforestring = examples[exampleNo].beforestring
+  var codeString = examples[exampleNo].codeString
+  var comment = examples[exampleNo].comment 
+  // var afterstring = examples[exampleNo].afterstring
+
+  resetSetup(beforestring, codeString,comment)
+  
+  // append(`#output-code`,afterstring,"o")
+  // append(`#afterhtml-code`,afterstring,"o")
+
+  exampleNo = exampleNo + 1
+
+  setTimeout(()=>{updateOutput()},2000)
+}
+
+
   load(["./md.scss"]);
 
 
+  
 
-
-
-function saveImage(){
-  // code to download #gridRoot as png;
-  console.log('pending export to png')
-}
-
+  async function saveImage(){
+    // code to download #gridRoot as png;
+    // console.log('pending export to png')
+    var elem = grab(`#saveCanvas`)[0]
+    var canvas = await html2canvas(elem)
+    var generatedfilename = `GeneratorJS-${grab(`#fileName`)[0].innerText}.png`
+    var link = await gen(a,`savepng`,"","button",{"href":canvas.toDataURL('image/png'),download:generatedfilename})
+    await link.click()
+    // append(app,link)
+    append(savepng,"","r")
+  
+  }
+  
+// load('savecanvas.js')
 
 
 append("#header",gen(header,"appheader","","header"),"r")
 
 load(["/defaults.js","/header.js"])
-window.$$ = GeneratorWebHelper()
-$$.PageNav().init()
-$$.loadCopyright();
+async function copyrightsloader(){
+   window.$$ =await  GeneratorWebHelper()
+   $$.PageNav().init()
+  await $$.loadCopyright();
+
+  setTimeout(()=>{
+    
+  var copyrightdiv= grab(`#copyright`)[0].outerHTML
+  append(copyright,"",'o')
+  append(saveCanvas,copyrightdiv)
+
+  },3000)
+}
+
+copyrightsloader()
